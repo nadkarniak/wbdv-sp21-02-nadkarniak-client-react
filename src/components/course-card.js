9c20b0d57c60926502d43749cef513888dbd32a9
nadkarniak <nadkarni.ak@gmail.com>
2021-02-20 01:24:12 -0500

    Editing course card

--- src/components/course-card.js
+++ src/components/course-card.js
@@ -9,17 +9,19 @@ const CourseCard = (
         lastModified,
         title,
         owner
-        }) =>{
-        const [editing, setEditing] = useState(false)
-        const [newTitle, setNewTitle] = useState(title)
-        const saveTitle = () => {
+    }) =>
+    {
+    const [editing, setEditing] = useState(false)
+    const [newTitle, setNewTitle] = useState(title)
+
+    const saveTitle = () => {
           setEditing(false)
           const newCourse = {
               ...course,
               title: newTitle
           }
           updateCourse(newCourse)
-        }
+      }
 
   return(
   <div className="col-4">
@@ -30,10 +32,19 @@ const CourseCard = (
         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's
           content.</p>
           <img src={``}/>
-        <Link to="/courses/editor" className="btn btn-primary">
+        {!editing && <Link to="/courses/editor" className="btn btn-primary">
             {course.title}
-        </Link>
+        </Link>}
+        {
+            editing &&
+            <input
+                onChange={(event) => setNewTitle(event.target.value)}
+                value={newTitle}
+                className="form-control"/>
+        }
         <i onClick={() => deleteCourse(course)} className="fas fa-trash"></i>
+        {!editing && <i onClick={() => setEditing(true)} className="fas fa-edit"></i>}
+        {editing && <i onClick={() => saveTitle()} className="fas fa-check"></i>}
       </div>
     </div>
   </div>

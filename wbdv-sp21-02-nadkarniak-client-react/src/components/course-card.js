8fcfbf954e4e49750dddcc2cee0dc4adbc4c9e5e
nadkarniak <nadkarni.ak@gmail.com>
2021-03-02 00:58:18 -0500

    Minor change

--- wbdv-sp21-02-nadkarniak-client-react/src/components/course-card.js
+++ wbdv-sp21-02-nadkarniak-client-react/src/components/course-card.js
@@ -32,7 +32,7 @@ const CourseCard = (
         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's
           content.</p>
           <img src={``}/>
-        {!editing && <Link to="/courses/editor" className="btn btn-primary">
+        {!editing && <Link to={`/courses/editor/${course._id}`} className="btn btn-primary">
             {course.title}
         </Link>}
         {

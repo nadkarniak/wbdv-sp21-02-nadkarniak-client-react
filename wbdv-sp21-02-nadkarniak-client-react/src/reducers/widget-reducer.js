f398f5f5c267ff340a5c295ca5f539f56d9cc000
nadkarniak <nadkarni.ak@gmail.com>
2021-03-21 16:11:32 -0400

    A5

--- wbdv-sp21-02-nadkarniak-client-react/src/reducers/widget-reducer.js
+++ wbdv-sp21-02-nadkarniak-client-react/src/reducers/widget-reducer.js
@@ -3,7 +3,7 @@ const initialState = {
 }
 
 const widgetReducer = (state=initialState, action) =>{
-    switch(action){
+    switch(action.type){
         case "CREATE_WIDGET":
             return {
                 ...state,
@@ -12,11 +12,33 @@ const widgetReducer = (state=initialState, action) =>{
                     action.widget
                 ]
             }
-        case "FIND_WIDGETS_FOR_TOPIC":
+        case "FIND_WIDGETS":
             return{
                 ...state,
                 widgets: action.widgets
             }
+        case "DELETE_WIDGET":
+            const newState1 = {
+                widgets: state.widgets.filter(widget => {
+                    if(widget._id === action.widgetToDelete._id){
+                        return false
+                    }
+                    else{
+                        return true
+                    }
+                })
+            }
+            return newState1
+        case "UPDATE_WIDGET":
+            return {
+                    widgets: state.widgets.map(w => {
+                        if(w._id === action.widget._id){
+                            return action.widget
+                        } else{
+                            return w
+                        }
+                    })
+                }
         default:
             return state
     }
